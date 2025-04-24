ad6bf834413696956a98416995b04d794793b8a0
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-04-24 22:20:55 +0530

    challenges of JS

---
+++ challenges/javascript_challenges/01_conditionals.js
@@ -0,0 +1,4 @@
+// You just need to implement the checkEvenOdd function
+function checkEvenOdd(num) {
+    // Return "Even" if num is even, otherwise return "Odd"
+}
---
+++ challenges/javascript_challenges/02_conditionals.js
@@ -0,0 +1,4 @@
+// You just need to implement the findLargest function
+function findLargest(a, b, c) {
+    // Return the largest among a, b, and c
+}
