87303f1e93d1ec6122165d1d44d673973550f17a
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-11-02 22:13:46 +0530

    Basic setup in Nodejs

--- NodeJsLearn/server.js
+++ NodeJsLearn/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './utils/db.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: `http://localhost:${PORT}`,
+    origin: process.env.BASE_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
-    
-}))
+}))// to allow cross-origin requests from specified origin 
+
+app.use(express.json()); // to parse JSON data in req.body (to accept JSON data from client)
+app.use(express.urlencoded({ extended: true })); // to parse form data in req.body () 
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello, Full Stack Project!');
